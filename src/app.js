8810b3b041c32b1b71ce03cbf15312abdeb63e9d
Vi The Quynh <vithequynh00@gmail.com>
2025-07-03 08:14:22 +0700

    thêm CRUD

--- src/app.js
+++ src/app.js
@@ -4,13 +4,10 @@ import postsRouter from "./routers/posts.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use("/api/posts", postsRouter);
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
--- src/routers/posts.js
+++ src/routers/posts.js
@@ -1,13 +1,67 @@
-import { Router } from "express";
+import express from 'express';
 
-const postsRouter = Router();
+const router = express.Router();
 
-postsRouter.get("/", (req, res) => {
-    res.json({ message: "Danh sách bài viết" });
+let posts = [
+    {id: 1, title: "bài viết 1", content: "Nội dung bài viết 1"},
+    {id: 2, title: "bài viết 2", content: "Nội dung bài viết 2"},
+];
+
+router.get("/", (req, res) => {
+    try {
+        const {search} = req.query;
+
+        let results = posts;
+
+        if (search) {
+            const keyword = search.toLowerCase();
+            results = posts.filter(post => 
+                post.title.toLowerCase().includes(keyword)
+            );
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ error: "Không tìm thấy bài viết nào" });
+        }
+
+         res.json(results);
+    } catch (error) {
+        res.status(500).json({ error: "Lỗi máy chủ" });
+    }
+   
 });
 
-postsRouter.post("/", (req, res) => {
-    res.json({ message: "Thêm bài viết mới" });
+router.get("/:id", (req, res) => {
+    const post = posts.find((p) => p.id == req.params.id);
+    if (!post) return res.status(404).json({ error: "Bài viết không tồn tại" });
+    res.json(post);
 });
 
-export default postsRouter;
+router.post("/", (req, res) => {
+    const { title, content } = req.body;
+    const newPost = { id : Date.now(), title, content };
+    posts.push(newPost);
+    res.status(201).json(newPost);
+});
+
+router.put("/:id", (req, res) => {
+    const post = posts.find((p) => p.id == req.params.id);
+    if (!post) return res.status(404).json({ error: "Bài viết không tồn tại" });
+
+    const { title, content } = req.body;
+    post.title = title || post.title;
+    post.content = content || post.content;
+
+    res.json(post);
+    
+});
+
+router.delete("/:id", (req, res) => {
+    const index = posts.findIndex((p) => p.id == req.params.id);
+    if(index === -1) return res.status(404).json({ error: "Bài viết không tồn tại" });
+
+    posts.splice(index, 1);
+    res.json({ success: true, message: "Bài viết đã được xóa" });
+});
+
+export default router;
+
